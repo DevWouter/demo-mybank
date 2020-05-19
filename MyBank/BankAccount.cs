c51ddfadd0ef8af5f1316a1a02c604fd5c799971
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-05-19 14:48:20 +0200

    change

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public virtual int Withdraw(int amount)
+        {
+            Balance -= amount;
+            return amount;
+        }
+
+        public override string ToString()
+        {
+            return "Balance is " + Balance;
+        }
+    }
+
+    class SavingsAccount : BankAccount
+    {
+        public override int Withdraw(int amount)
+        {
+            if (amount > Balance)
+            {
+                throw new Exception("You are overdrafting your savings account");
+            }
+
+            return base.Withdraw(amount);
+        }
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new SavingsAccount();
+            myAccount.Balance = 0;
+            myAccount.Deposit(500);
+            myAccount.Withdraw(200);
+
+            Console.WriteLine(myAccount);
+            // Console.ReadLine();  
+        }
+    }
+}
